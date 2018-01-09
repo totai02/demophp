8815e34f0e4518b655f998cfd11a529f5de36fcb
haidt <haidt@sandinh.net>
2018-01-09 13:54:19 +0700

    fix define admin.php

--- admin.php
+++ admin.php
@@ -3,15 +3,17 @@ session_start();
 
 error_reporting(E_ALL);
 
-define('DIR_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/');
+$sub = '/';
+
+define('DIR_ROOT', $_SERVER['DOCUMENT_ROOT'] . $sub);
 define('DIR_APP', DIR_ROOT . 'admin/');
 define('DIR_SYSTEM', DIR_ROOT . 'system/');
 define('DIR_TEMPLATE', DIR_APP . 'view/');
 define('DIR_RESOURCES', DIR_ROOT . 'resources/');
 define('DIR_IMAGE', DIR_RESOURCES . 'upload/image/');
 
-define('HTTP_ROOT', 'http://' . $_SERVER['HTTP_HOST'] . '/');
-define('HTTP_APP', 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
+define('HTTP_ROOT', 'http://' . $_SERVER['HTTP_HOST'] . $sub);
+define('HTTP_APP', HTTP_ROOT . 'admin.php');
 
 include_once(DIR_SYSTEM . 'config.php');
 include_once(DIR_SYSTEM . 'bootstrap.php');
@@ -43,12 +45,13 @@ $document = new Document();
 
 if (isset($_GET['route'])) {
     $route = $_GET['route'];
+
     $page = 'error/not_found';
 
     if (in_array($route, $config->get('local.config.ignore'))) {
         $page = $route;
     } else {
-        if (isset($_SESSION['user_id'])) {
+        if ($user->isLogin()) {
             if (file_exists(DIR_APP . 'controller/' . $route . '.php')) {
                 $page = $route;
             }
