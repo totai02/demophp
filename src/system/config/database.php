dd331ea564ff08e9f7d893c2040120943f26e68c
Thanh Pham Van <thanhpv@sandinh.net>
2018-01-11 03:06:09 +0700

    recontruct entire project, add more conf file and changes to docker-compose.yml

---
+++ src/system/config/database.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', $_ENV["DB_HOST"]);
+define('DB_PORT', $_ENV["DB_PORT"]);
+define('DB_USER', $_ENV["DB_USER"]);
+define('DB_PASSWORD', $_ENV["DB_PASSWORD"]);
+define('DB_NAME', $_ENV["DB_NAME"]);
+define('DB_PREFIX', 'tt_');
--- system/config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_HOST', 'database');
-define('DB_PORT', '3306');
-define('DB_USER', 'mysql');
-define('DB_PASSWORD', 'mysql');
-define('DB_NAME', 'demo_shop');
-define('DB_PREFIX', 'tt_');
